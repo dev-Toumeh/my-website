9c48a9d32d898353c0f05beb6b356771c1af0c3d
toumeh <dev.toumeh@gmail.com>
2024-03-27 20:03:55 +0100

    modify contact template and add javascript logic
    -create the contact form using fluid template and add it in the partials
    - add javascript logic to handle the the post request, and privent
    reloading the page

--- Resources/Public/js/scripts.js
+++ Resources/Public/js/scripts.js
@@ -5,13 +5,43 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-const removeFluidTemplateDefaultAttributes = (element, attributes) => {
-    attributes.forEach(attr => {
-        if(element.hasAttribute(attr)) {
-            element.removeAttribute(attr);
-        }
-    });
-}
+// const removeFluidTemplateDefaultAttributes = (element, attributes) => {
+//     attributes.forEach(attr => {
+//         if(element.hasAttribute(attr)) {
+//             element.removeAttribute(attr);
+//         }
+//     });
+// }
+//
+// const profilePhotoElement = document.getElementById('myprofile');
+// removeFluidTemplateDefaultAttributes(profilePhotoElement, ['height', 'width']);
+//
 
-const profilePhotoElement = document.getElementById('myprofile');
-removeFluidTemplateDefaultAttributes(profilePhotoElement, ['height', 'width']);
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('contactForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const formData = new FormData(this);
+
+        fetch('/contact/json', {
+            method: 'POST',
+            body: formData,
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    this.reset();
+                    // Display the success message
+                    document.getElementById('submitSuccessMessage').classList.remove('d-none');
+                } else {
+                    // Handle failure
+                    document.getElementById('submitErrorMessage').classList.remove('d-none');
+                }
+            })
+            .catch(error => {
+                // Handle errors here
+                console.error('Error:',  error);
+                document.getElementById('submitErrorMessage').classList.remove('d-none');
+            });
+    });
+});
