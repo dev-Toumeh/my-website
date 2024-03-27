dbc481f6ef0b17a5f9c03b0be9d4731f0fed88c2
toumeh <dev.toumeh@gmail.com>
2024-03-27 21:02:18 +0100

    printing error message in the contact page is now dynamic

--- Resources/Public/js/scripts.js
+++ Resources/Public/js/scripts.js
@@ -16,32 +16,34 @@
 // const profilePhotoElement = document.getElementById('myprofile');
 // removeFluidTemplateDefaultAttributes(profilePhotoElement, ['height', 'width']);
 //
+if (window.location.pathname === '/contact') {
+    document.addEventListener('DOMContentLoaded', (event) => {
+        document.getElementById('contactForm').addEventListener('submit', function (e) {
+            e.preventDefault();
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('contactForm').addEventListener('submit', function(e) {
-        e.preventDefault();
+            const formData = new FormData(this);
 
-        const formData = new FormData(this);
-
-        fetch('/contact/json', {
-            method: 'POST',
-            body: formData,
-        })
-            .then(response => response.json())
-            .then(data => {
-                if (data.success) {
-                    this.reset();
-                    // Display the success message
-                    document.getElementById('submitSuccessMessage').classList.remove('d-none');
-                } else {
-                    // Handle failure
-                    document.getElementById('submitErrorMessage').classList.remove('d-none');
-                }
+            const defaultMessage = "An unexpected error occurred. Please try again later.";
+            fetch('/contact/json', {
+                method: 'POST',
+                body: formData,
             })
-            .catch(error => {
-                // Handle errors here
-                console.error('Error:',  error);
-                document.getElementById('submitErrorMessage').classList.remove('d-none');
-            });
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        this.reset();
+                        document.getElementById('submitSuccessMessage').classList.remove('d-none');
+                    } else {
+                        document.getElementById('submitErrorMessage').classList.remove('d-none');
+                        document.getElementById('error-message').textContent = data.errorMessage || defaultMessage;
+                    }
+                })
+                .catch(error => {
+                    // Handle errors here
+                    console.error('Error:', error);
+                    document.getElementById('error-message').textContent = defaultMessage;
+                    document.getElementById('submitErrorMessage').classList.remove('d-none');
+                });
+        });
     });
-});
+}
